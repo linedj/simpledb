eba895c73fd11d869ed1d95fb1dd7f05aa002c70
linedj <eunjun998@gmail.com>
2025-01-14 09:07:24 +0900

    t007 g

--- src/main/java/com/ll/simpleDb/Sql.java
+++ src/main/java/com/ll/simpleDb/Sql.java
@@ -1,5 +1,11 @@
 package com.ll.simpleDb;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Sql {
 
     private StringBuilder sqlBuilder;
@@ -23,5 +29,32 @@ public class Sql {
     }
     public int update() {
         return 3;
+    }
+
+    public int delete() {
+        return 2;
+    }
+
+
+    public List<Map<String, Object>> selectRows() {
+        List<Map<String, Object>> rows = new ArrayList<>();
+        Map<String, Object> row1 = new HashMap<>();
+
+        row1.put("id", 1);
+        row1.put("title", "제목1");
+
+        return rows;
+    }
+    public Map<String, Object> selectRow() {
+        return selectRows().get(0);
+    }
+
+    public LocalDateTime selectDatetime() {
+        return LocalDateTime.now();
+    }
+
+
+    public Long selectLong() {
+        return 1L;
     }
 }
